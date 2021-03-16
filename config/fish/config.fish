cad0546e6ff3f574f816c09aed8af1741f43c738
Jonas Helgemo <jonas@jhel.no>
2021-03-16 14:55:32 +0100

    Add curl installer

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,53 +3,33 @@ if not set -q fish_shell_initialized
     echo -n Setting abbreviations...
 
     abbr alpine-fish 'docker run -v (pwd):/local-dir --rm --name alpine-fish -it colstrom/abbr'
-    abbr bbi 'brew install'
     abbr be 'bundle exec'
     abbr bi 'bundle install'
-    abbr brm 'brew uninstall'
     abbr bs 'brew search'
-    abbr cht 'cht.sh'
-    abbr cht-shell 'cht.sh --shell'
-    abbr dap "dotnet add package"
     abbr dcl 'docker-compose logs -f'
     abbr dco 'docker-compose'
     abbr dcu 'docker-compose up -d'
-    abbr ssh-kg-ed25519 'ssh-keygen -a 100 -t ed25519 -C "jonas.helgemo@webstep.no" -f id_ed25519'
-    abbr ssh-kg-rsa 'ssh-keygen -a 100 -t rsa -C "jonas.helgemo@webstep.no" -f id_rsa'
-    abbr dr "dotnet run"
-    abbr gv 'tmuxinator start gv'
-    abbr i 'idea'
     abbr planck-flash 'wally-cli (ls -t ~/Downloads/planck*.bin | head -1)'
     abbr si 'say -v Nora'
     abbr st 'git status'
     abbr ubuntu 'docker run -it --rm ubuntu'
     abbr ubuntu-fish 'docker run -v (pwd):/local-dir --rm --name ubuntu-fish -it colstrom/fish:ubuntu'
-    abbr vsh 'vaulted shell -n'
     abbr weeknumber 'gdate +%V -d today '
-    abbr dev-proxy 'c'
-    abbr plex-aws-start 'aws ec2 start-instances --instance-ids "i-0c7f09bb38bc59b1a"'
-    abbr plex-aws-stop 'aws ec2 stop-instances --instance-ids "i-0c7f09bb38bc59b1a"'
-    abbr plex-aws-status 'aws ec2 describe-instance-status --instance-ids "i-0c7f09bb38bc59b1a"'
     echo 'Done ✨'
+
 end
 
 # Set paths
 set -gx PATH $PATH /usr/sbin /usr/bin /sbin /bin $HOME/bin /usr/local/bin $HOME/.dapr/bin $HOME/src/go/bin $HOME/.cargo/bin $HOME/.asdf/shims/ $HOME/.asdf/bin/ $HOME/.asdf/installs/rust/stable/bin/ /Applications/Emacs.app/Contents/MacOS/ /Applications/Emacs.app/Contents/MacOS/bin/ $HOME/.emacs.d/bin/ ~/.local/bin
 
 # Set CDPATH
-set -gx CDPATH $CDPATH .
-
-# Set Stowdir
-set -gx STOW_DIR $HOME/dotfiles
+#set -gx CDPATH $CDPATH .
 
 # Golang
 set -gx GOPATH $HOME/src/go
 
 # Editor
 set -gx EDITOR 'emacsclient -n -create-frame --alternate-editor=""'
-
-# Java home
-set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-11.0.1.jdk/Contents/Home
 
 # z.lua
 set -gx _ZL_MATCH_MODE 1
@@ -59,15 +39,6 @@ set -gx AZURE_CONFIG_DIR ~/.azure
 
 # GnuPG
 set -gx GPG_TTY (tty)
-
-
-# fzf use fd
-#set -gx FZF_DEFAULT_OPS '--height=70% --preview="cat {}" --preview-window=right:60%:wrap'
-#set -gx FZF_DEFAULT_COMMAND 'set'
-#set -gx FZF_CTRL_T_COMMAND 'fd --type f'
-# set -gx FZF_ALT_C_COMMAND 'fd --ntype d'
-# set -gx FZF_LEGACY_KEYBINDINGS 0
-# set -gx FZF_COMPLETE 0
 
 # fzf-fish bindings
 # \cf is Ctrl+f
@@ -81,17 +52,22 @@ bind \e\cj '__fzf_search_git_status'
 
 # Aliases
 alias g='hub'
-alias bb='cat (string match -v .Brewfile.lock.json -- .Brewfile*) | brew bundle --file=-'
-alias git='hub'
+alias bb='brew bundle --global'
 alias tf='terraform'
 alias uuid='uuidgen  | awk \'{print tolower($0)}\''
 alias cat='bat'
 alias less='bat'
 alias more='bat'
-alias sed='gsed'
 alias ls='exa'
-alias atari='hatari -c ~/.config/hatari/hatari.cfg'
-alias notifyme='echo "✅" | mail -s "🦄🌈✨" jonas.helgemo@webstep.no'
+alias top='ytop'
+alias htop='ytop'
+alias ps='procs'
+alias sed='sd'
+alias du='dust'
+alias grep='ripgrep'
+alias tldr='tealdeer'
+
+alias dev-proxy='caddy run --config ~/.caddy/Caddyfile'
 
 ################################################################################
 # Load customer custom config if present in customer src folder
@@ -107,9 +83,6 @@ source /usr/local/opt/asdf/asdf.fish
 
 # zoxide
 zoxide init fish | source
-
-# https://github.com/wfxr/forgit git extras
-source ~/.config/fish/plugins/forgit.plugin.fish
 
 # Starship must be last
 eval (starship init fish)

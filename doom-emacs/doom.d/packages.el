0bf2059ba47e54afd24cb92d72a36fd142dd7f31
Jonas Helgemo <jonas@jhel.no>
2021-03-19 16:07:04 +0100

    More packages added to emacs

--- doom-emacs/doom.d/packages.el
+++ doom-emacs/doom.d/packages.el
@@ -15,6 +15,8 @@
 (package! key-chord)
 (package! fish-mode)
 (package! org-gcal)
+(package! yaml-mode)
+(package! ssh-config-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
